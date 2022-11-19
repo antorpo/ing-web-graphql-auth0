d726172a1431376753b823bea0464f57d91d0b22
Antonio Gonzalez <antonio.gonzalezr@udea.edu.co>
2022-11-19 00:25:44 -0500

    Ajuste URL

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 export const clientGraph = new ApolloClient({
-  uri: process.env.SERVER_URL ?? "http://localhost:3000/api/graphql",
+  uri: process.env.SERVER_URL ?? "https://ing-web-graphql-auth0.vercel.app/api/graphql",
   cache: new InMemoryCache(),
 });
 
